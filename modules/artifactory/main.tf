527d50b53cf3659bb00221ff624468b49c765d26
Oleksii Taranyk <oleksii.taranyk@gmail.com>
2024-05-31 13:23:09 +0300

    docker run artifactory

--- modules/artifactory/main.tf
+++ modules/artifactory/main.tf
@@ -22,7 +22,39 @@ resource "google_compute_instance" "artifactory" {
   }
 
   metadata = <<EOT
-        ls  
+        #!/bin/bash
+
+        export JFROG_HOME=/opt/jfrog
+        sudo mkdir -p \$JFROG_HOME
+        sudo chown -R \$(whoami):\$(whoami) \$JFROG_HOME
+       
+        mkdir -p $JFROG_HOME/artifactory/var/etc/
+        cd $JFROG_HOME/artifactory/var/etc/
+        touch ./system.yaml
+        chown -R 1030:1030 $JFROG_HOME/artifactory/var
+        chmod -R 777 $JFROG_HOME/artifactory/var
+
+        sudo apt-get update
+        sudo apt-get install -y \
+            apt-transport-https \
+            ca-certificates \
+            curl \
+            gnupg \
+            lsb-release
+
+        curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+        echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+        sudo apt-get update
+        sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+        sudo systemctl start docker
+        sudo systemctl enable docker
+
+        sudo docker run --name artifactory -v $JFROG_HOME/artifactory/var/:/var/opt/jfrog/artifactory -d -p 8081:8081 -p 8082:8082 releases-docker.jfrog.io/jfrog/artifactory-oss:latest
     EOT
 
   tags = ["artifactory"]
