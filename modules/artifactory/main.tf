ffd16ba94f87a136a57f8eceb327274524456902
Oleksii Taranyk <oleksii.taranyk@gmail.com>
2024-05-31 13:01:05 +0300

    artifactory main.tf

---
+++ modules/artifactory/main.tf
@@ -0,0 +1,49 @@
+provider "google" {
+    project = var.project
+    region  = var.region
+}
+
+resource "google_compute_instance" "artifactory" {
+    name = "artifactory-vm"
+    machine_type = var.machine_type
+    zone = var.zone
+
+    boot_disk {
+        initialize_params {
+        image = "ubuntu-os-cloud/ubuntu-2204-lts"
+        }
+    }
+
+    network_interface {
+        network = "default"
+        access_config {
+            nat_ip = google_compute_address.artifactory_ip.address
+        }
+    }
+
+    metadata = <<EOT
+        ls  
+    EOT
+
+    tags = ["artifactory"]
+}
+
+resource "google_compute_address" "artifactory_ip" {
+    name = "artifactory-ip"
+    region = var.region
+}
+
+resource "google_compute_firewall" "artifactory_firewall" {
+    name = "artifactory-firewall"
+    network = "default"
+    source_ranges = ["0.0.0.0/0"]
+    target_tags = ["artifactory"]
+    allow {
+        protocol = "tcp"
+        ports = ["8081"]
+    }  
+}
+
+output "artifactory_ip" {
+    value = google_compute_address.artifactory_ip.address
+}
