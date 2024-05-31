bc04fd04f35c9b5f94323b80f169f3230d1ec336
Oleksii Taranyk <oleksii.taranyk@gmail.com>
2024-05-31 13:26:34 +0300

    insert artifactory in main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,29 @@
 
 provider "google" {
   project = var.project
-  region = var.region
-  zone = var.zone 
+  region  = var.region
+  zone    = var.zone
 }
-
-
-
 
 data "google_compute_network" "default" {
   name = "default"
 }
 
-
 module "pq" {
 
-    source = "./modules/postgresql"
-  
+  source = "./modules/postgresql"
+
 }
 
 module "servers" {
-    source = "./modules/servers"   
-    region = var.region
+  source = "./modules/servers"
+  region = var.region
 }
-
-
-
 
 module "monitoring" {
   source = "./modules/monitoring"
+}
+
+module "artifactory" {
+  source = "./modules/artifactory"
 }
